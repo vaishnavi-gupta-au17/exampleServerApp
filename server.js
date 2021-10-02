ad8cb00cc66770201dd878ed3d18c6db06ff357b
vaishnavi-gupta-au17 <vaishnaviguptampi1@gmail.com>
2021-10-02 13:58:44 +0530

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server Working. This is a Homepage");
+  res.send("Oh Hello");
 });
 
 console.log(`Heroku Port No. Is ${process.env.PORT}`);
